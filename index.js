969ec5b911b302e9bb511ed037f4952facfad9e9
damilolaakinbi <akinbidamilola074@gmail.com>
2023-01-18 23:34:24 +0100

    Completed. POST request exercise done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Add your code here
+function submitData(username, userEmail) {
+  return fetch(`http://localhost:3000/users`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name: username, email: userEmail }),
+  })
+    .then((resp) => resp.json())
+    .then((data) => data)
+    .catch((error) => console.log(error.message));
+}
